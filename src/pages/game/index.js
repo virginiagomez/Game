59fca4ac8fc2457b004ff0df08d46a05492a4f90
Paul <paulsanchezg24@gmail.com>
2022-10-11 15:21:59 -0500

    adds user selection

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -4,13 +4,29 @@ import {ReactComponent as ReacticonPaper} from '../../assets/svg/iconPaper.svg'
 import {ReactComponent as ReacticonScissors} from '../../assets/svg/iconScissors.svg'
 import Youwin from "../youwin";
 
+const gameOptions = [
+  {
+    name: 'Rock',
+    id: 'rock'
+  },
+  {
+    name: 'Paper',
+    id: 'paper'
+  },
+  {
+    name: 'Scissors',
+    id: 'scissors'
+  },
+]
+
 const Game = (props) => {
 
   const [counter, setCounter] = useState(3);
   const [isFinished, setIsFinished] = useState(false);
+  const [userSelection, setUserSelection] = useState(null);
 
   useEffect(()=>{
-    if(counter < 0) {setIsFinished (true)}
+    if(counter < 0) return (setIsFinished (true))
     //disminuye un segundo al contador
     const interval = setInterval(() => {
       setCounter(prev => prev = prev - 1)
@@ -18,22 +34,49 @@ const Game = (props) => {
     return () => clearInterval(interval);
   },[counter])
 
+  //id === 'rock' -> handleUserSelection(option.id)
+  //id === {name: 'Rock', id: 'rock'} -> handleUserSelection(option)
+  function handleUserSelection(id) {
+    setUserSelection(id)
+  }
+
   return isFinished ? 
-    <Youwin/>
+    <Youwin />
     :
     <div style={{display:"flex",flexDirection:"column",alignItems:"center"}}>
     <div className='iconsContainer'>
-      <div><ReacticonRock className='icons'/></div>
-      <div><ReacticonPaper className='icons'/></div>
-      <div><ReacticonScissors className='icons'/></div>
+      {gameOptions.map((option, index) =>{
+        return <div key={`option_${option.id}`}>
+          {option.id === 'rock' ? 
+            <ReacticonRock className='icons'/>
+            : option.id === 'scissors' ? 
+            <ReacticonScissors className='icons'/>
+            : 
+            <ReacticonPaper className='icons'/>
+          }
+        </div>
+      })}
     </div>
     <div className='iconsContainer'>
       <div className='number'>{counter}</div>
     </div>
     <div className='iconsContainer'>
-      <div> <ReacticonRock className='icons'/></div>
-      <div><ReacticonPaper className='icons'/></div>
-      <div><ReacticonScissors className='icons'/></div>
+      {gameOptions.map((option, index) =>{
+        return (
+          <div 
+            onClick={()=>{handleUserSelection(option.id)}}
+            key={`option_${option.id}`}
+          >
+            {option.id === 'rock' ? 
+              <ReacticonRock style={{border: userSelection === option.id ? '8px solid green':''}} className='icons'/>
+              : option.id === 'scissors' ? 
+              <ReacticonScissors style={{border: userSelection === option.id ? '8px solid green':''}} className='icons'/>
+              : 
+              <ReacticonPaper style={{border: userSelection === option.id ? '8px solid green':''}} className='icons'/>
+            }
+          </div>
+        )
+      })}
     </div>
   </div>
 };
